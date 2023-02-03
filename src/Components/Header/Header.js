a70cec02433c143fa6e2bc531ff57ea623f59190
pranav s <dev.pranabsk@gmail.com>
2023-02-03 16:27:02 +0530

    5. Using Redux for Authentication Token: login on auth

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,8 +3,48 @@ import "./Header.css";
 import { Navbar, NavbarBrand, Nav, NavItem } from "reactstrap";
 import { NavLink } from "react-router-dom";
 import Logo from "../../assets/logo.png";
+import { connect } from "react-redux";
 
-const Header = () => {
+// state access by redux
+const mapStateToProps = (state) => {
+  return {
+    token: state.token
+  };
+};
+
+const Header = (props) => {
+  let links = null;
+  if(props.token === null){
+    links = (
+      <Nav className="mr-md-5">
+      <NavItem>
+        <NavLink exact to="/login" className="NavLink">
+          Login
+        </NavLink>
+      </NavItem>
+    </Nav>
+    )
+  }else{
+    links = (
+      <Nav className="mr-md-5">
+      <NavItem>
+        <NavLink exact to="/" className="NavLink">
+          Something
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink exact to="/orders" className="NavLink">
+          Orders
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink exact to="/logout" className="NavLink">
+          Logout
+        </NavLink>
+      </NavItem>
+    </Nav>
+    )
+  }
   return (
     <div className="Navigation">
       <Navbar
@@ -17,27 +57,11 @@ const Header = () => {
           <NavbarBrand href="/" className="mr-auto ml-md-5 Brand">
             <img src={Logo} alt="Logo" width="80px" />
           </NavbarBrand>
-          <Nav className="mr-md-5">
-            <NavItem>
-              <NavLink exact to="/" className="NavLink">
-                Something
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink exact to="/orders" className="NavLink">
-                Orders
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink exact to="/login" className="NavLink">
-                Login
-              </NavLink>
-            </NavItem>
-          </Nav>
+         {links}
         </div>
       </Navbar>
     </div>
   );
 };
 
-export default Header;
+export default connect(mapStateToProps)(Header);
