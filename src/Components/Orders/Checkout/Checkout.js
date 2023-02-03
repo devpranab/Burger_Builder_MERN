f95dc14727d4bfc51f9b8dcd4d6d3303fa299d97
pranav s <dev.pranabsk@gmail.com>
2023-02-03 13:13:02 +0530

    13. Improving Checkout Component

--- src/Components/Orders/Checkout/Checkout.js
+++ src/Components/Orders/Checkout/Checkout.js
@@ -1,124 +1,172 @@
-import React, { Component } from 'react';
-import { Button } from 'reactstrap';
+import React, { Component } from "react";
+import { Button, Modal, ModalBody } from "reactstrap";
 import axios from "axios";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 import { resetIngredients } from "../../../redux/actionCreators";
+import Spinner from "../../Spinner/Spinner";
 
-const mapStateToProps = state => {
-    return {
-        ingredients: state.ingredients,
-        totalPrice: state.totalPrice,
-        purchasable: state.purchasable
-    };
+const mapStateToProps = (state) => {
+  return {
+    ingredients: state.ingredients,
+    totalPrice: state.totalPrice,
+    purchasable: state.purchasable,
+  };
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-        resetIngredients: () => dispatch(resetIngredients())
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {
+    resetIngredients: () => dispatch(resetIngredients()),
+  };
 };
 
 class Checkout extends Component {
-    state = {
-        values: {
-            deliveryAddress: "",
-            phone: "",
-            paymentType: "Cash On Delivery",
+  state = {
+    values: {
+      deliveryAddress: "",
+      phone: "",
+      paymentType: "Cash On Delivery",
+    },
+    isLoading: false,
+    isModalOpen: false,
+    modalMsg: ""
+  };
+
+  // goBack function
+  goBack = () => {
+    this.props.history.goBack("/");
+  };
+
+  // inputChangerHandler function
+  inputChangerHandler = (e) => {
+    this.setState({
+      values: {
+        ...this.state.values,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
+  // submitHandler
+  submitHandler = () => {
+    console.log(this.state.values);
+    const order = {
+      //order post to database(firebase)
+      ingredients: this.props.ingredients,
+      customer: this.state.values,
+      price: this.props.totalPrice,
+      orderTime: new Date(),
+    };
+    console.log(order);
+    axios
+      .post(
+        "https://burger-builder-5ba58-default-rtdb.firebaseio.com/orders.json",
+        order
+      )
+      .then((response) => {
+        if (response.status === 200) {
+            this.setState({
+                isLoading: false,
+                isModalOpen: true,
+                modalMsg: "Order Placed Successfully!",
+            });
+         
+        } else {
+            this.setState({
+                isLoading: false,
+                isModalOpen: true,
+                modalMsg: "Something Went Wrong! Order Again!",
+            });
         }
-    };
-
-    // goBack function
-    goBack = () => {
-        this.props.history.goBack("/");
-    };
-
-    // inputChangerHandler function
-    inputChangerHandler = e => {
+    })
+    .catch((err) => {
         this.setState({
-            values: {
-                ...this.state.values,
-                [e.target.name]: e.target.value
-            }
+            isLoading: false,
+            isModalOpen: true,
+            modalMsg: "Something Went Wrong! Order Again!",
         });
-    };
-
-    // submitHandler 
-    submitHandler = () => {
-        console.log(this.state.values);
-        const order = {    //order post to database(firebase)
-            ingredients: this.props.ingredients,
-            customer: this.state.values,
-            price: this.props.totalPrice,
-            orderTime: new Date(),
-        };
-        console.log(order);
-        axios
-        .post("https://burger-builder-5ba58-default-rtdb.firebaseio.com/orders.json", order)
-        .then(response => console.log(response))
-        .catch(err => console.log(err));
-
-        alert("Saving Orders to Database!");
-    }
-
-    render() {
-        return (
-            <div>
-                   <h4
-                    style={{
-                        border: "1px solid grey",
-                        boxShadow: "1px 1px #888888",
-                        borderRadius: "5px",
-                        padding: "20px",
-                    }}>
-                    Payment: {this.props.totalPrice} INR
-                </h4>
-                <form
-                    style={{
-                        border: "1px solid grey",
-                        boxShadow: "1px 1px #888",
-                        borderRadius: "5px",
-                        padding: "20px",
-                    }}>
-                    <textarea
-                        name="deliveryAddress"
-                        value={this.state.values.deliveryAddress}
-                        className="form-control"
-                        placeholder="Your Address"
-                        onChange={(e) => this.inputChangerHandler(e)}
-                    ></textarea>
-                    <br />
-                    <input
-                        name="phone"
-                        className="form-control"
-                        value={this.state.values.phone}
-                        placeholder="Your Phone Number"
-                        onChange={(e) => this.inputChangerHandler(e)}
-                    />
-                    <br />
-                    <select
-                        name="paymentType"
-                        className="form-control"
-                        value={this.state.values.paymentType}
-                        onChange={(e) => this.inputChangerHandler(e)}>
-                        <option value="Cash On Delivery">Cash On Delivery</option>
-                        <option value="Phonepe">Phonepe</option>
-                        <option value="Credit Card">Credit Card</option>
-                    </select>
-                    <br />
-                    <Button
-                        style={{ backgroundColor: "#D70F64" }} onClick={this.submitHandler} disabled={!this.props.purchasable}>
-                        Place Order
-                    </Button>
-                    <Button
-                    style={{marginLeft: "10px"}}
-                        color="secondary" onClick={this.goBack}>
-                        Cancel
-                    </Button>
-                
-                </form>
-            </div>
-        );
-    }
+    });
+};
+render() {
+    let form = (
+        <div>
+            <h4
+                style={{
+                    border: "1px solid grey",
+                    boxShadow: "1px 1px #888888",
+                    borderRadius: "5px",
+                    padding: "20px",
+                }}
+            >
+                Payment: {this.props.totalPrice} BDT
+            </h4>
+            <form
+                style={{
+                    border: "1px solid grey",
+                    boxShadow: "1px 1px #888888",
+                    borderRadius: "5px",
+                    padding: "20px",
+                }}
+            >
+                <textarea
+                    name="deliveryAddress"
+                    value={this.state.values.deliveryAddress}
+                    className="form-control"
+                    placeholder="Your Address"
+                    onChange={(e) => this.inputChangerHandler(e)}
+                ></textarea>
+                <br />
+                <input
+                    name="phone"
+                    className="form-control"
+                    value={this.state.values.phone}
+                    placeholder="Your Phone Number"
+                    onChange={(e) => this.inputChangerHandler(e)}
+                />
+                <br />
+                <select
+                    name="paymentType"
+                    className="form-control"
+                    value={this.state.values.paymentType}
+                    onChange={(e) => this.inputChangerHandler(e)}
+                >
+                    <option value="Cash On Delivery">
+                        Cash On Delivery
+                    </option>
+                    <option value="Bkash">Bkash</option>
+                </select>
+                <br />
+                <Button
+                    style={{ backgroundColor: "#D70F64" }}
+                    className="mr-auto"
+                    onClick={this.submitHandler}
+                    disabled={!this.props.purchasable}
+                >
+                    Place Order
+                </Button>
+                <Button
+                style={{
+                    marginLeft: "5px"
+                }}
+                    color="secondary"
+                    className="ml-1"
+                    onClick={this.goBack}
+                >
+                    Cancel
+                </Button>
+            </form>
+        </div>
+    );
+    return (
+        <div>
+            {this.state.isLoading ? <Spinner /> : form}
+            <Modal isOpen={this.state.isModalOpen} onClick={this.goBack}>
+                <ModalBody>
+                    <p>{this.state.modalMsg}</p>
+                </ModalBody>
+            </Modal>
+        </div>
+    );
+ }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Checkout);
