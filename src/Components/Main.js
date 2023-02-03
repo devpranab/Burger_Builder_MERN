8086226cb7b8c35b8f854092084e571b70ece837
pranav s <dev.pranabsk@gmail.com>
2023-02-03 16:38:38 +0530

    5. Using Redux for Authentication Token: components show on auth

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,23 +1,46 @@
 import React from "react";
 import Header from "./Header/Header";
 import BurgerBuilder from "./BurgerBuilder/BurgerBuilder";
-import { Route } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Orders from "./Orders/Orders";
 import Checkout from "./Orders/Checkout/Checkout";
 import Auth from "../Auth/Auth";
+import { connect } from "react-redux";
 
-const Main = () => {
+// state access by redux
+const mapStateToProps = (state) => {
+  return {
+    token: state.token,
+  };
+};
+
+const Main = (props) => {
+  let ourRoutes = null;
+  if (props.token === null) {
+    ourRoutes = (
+      <Switch>
+        <Route path="/login" component={Auth} />
+        <Redirect to="/login" />
+      </Switch>
+    );
+  } else {
+    ourRoutes = (
+      <Switch>
+        <Route path="/orders" component={Orders} />
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/" />
+      </Switch>
+    );
+  }
   return (
     <div>
       <Header />
       <div className="container pt-5" style={{ minHeight: "75.4vh" }}>
-            <Route path="/orders" component={Orders} />
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/login" component={Auth} />
-            <Route path="/" exact component={BurgerBuilder} />
-        </div>
+        {ourRoutes}
+      </div>
     </div>
   );
 };
 
-export default Main;
+export default connect(mapStateToProps)(Main);
