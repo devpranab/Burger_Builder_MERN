2bd5ad6875262cd9414d0587045a479551fc6120
pranav s <dev.pranabsk@gmail.com>
2023-02-03 13:38:38 +0530

    15. Completing Orders Component

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchOrders } from "../../redux/actionCreators";
-//import Spinner from "../Spinner/Spinner";
 
-const mapStateToProps = state => {
+import Order from "./Order/Order";
+import Spinner from "../Spinner/Spinner";
+
+const mapStateToProps = (state) => {
   return {
     orders: state.orders,
     orderLoading: state.orderLoading,
@@ -11,7 +13,7 @@ const mapStateToProps = state => {
   };
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
     fetchOrders: () => dispatch(fetchOrders()),
   };
@@ -25,12 +27,44 @@ class Orders extends Component {
     console.log(this.props);
   }
   render() {
-    return (
-        <div>
-          <p>Orders</p>
-        </div>
-    ) 
- }
+    let orders = null;
+    if (this.props.orderErr) {
+      orders = (
+        <p
+          style={{
+            border: "1px solid grey",
+            boxShadow: "1px 1px #888888",
+            borderRadius: "5px",
+            padding: "20px",
+            marginBottom: "10px",
+          }}
+        >
+          Sorry Failed to Load Orders!
+        </p>
+      );
+    } else {
+      if (this.props.orders.length === 0) {
+        orders = (
+          <p
+            style={{
+              border: "1px solid grey",
+              boxShadow: "1px 1px #888888",
+              borderRadius: "5px",
+              padding: "20px",
+              marginBottom: "10px",
+            }}
+          >
+            You have no Orders!
+          </p>
+        );
+      } else {
+        orders = this.props.orders.map((order) => {
+          return <Order order={order} key={order.id} />;
+        });
+      }
+    }
+    return <div>{this.props.orderLoading ? <Spinner /> : orders}</div>;
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Orders);
